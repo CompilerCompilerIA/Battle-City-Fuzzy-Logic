e12600b5fb3f2124bd1f3f43cc7bf3112884bf8f
Santiago Alejandro Vargas Céspedes <60782047+AleS900@users.noreply.github.com>
2022-04-29 15:03:54 -0400

    Update AnimatedMouseListener.java

--- src/main/java/app/animation/AnimatedMouseListener.java
+++ src/main/java/app/animation/AnimatedMouseListener.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2014 Saúl Piña <sauljabin@gmail.com>.
- * <p>
- * This file is part of FuzzyBattle.
- * <p>
- * FuzzyBattle is licensed under The MIT License.
- * For full copyright and license information please see the LICENSE file.
- */
 
 package app.animation;
 
